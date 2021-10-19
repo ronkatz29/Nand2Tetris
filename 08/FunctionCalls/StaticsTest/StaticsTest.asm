d1570739668e0e191297317e0d431d7d19fe184a
ronkatz29 <ronkatz29@gmail.com>
2021-10-20 01:22:46 +0300

    try test for 8

--- 08/FunctionCalls/StaticsTest/StaticsTest.asm
+++ 08/FunctionCalls/StaticsTest/StaticsTest.asm
@@ -1,3 +1,58 @@
+//Init command
+	@256
+	D=A
+	@SP
+	M=D
+	@RETURN_ADDRESS_0
+	D=A
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@LCL
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@ARG
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@THIS
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@THAT
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@SP
+	D=M
+	@5
+	D=D-A
+	@0
+	D=D-A
+	@ARG
+	M=D
+	@SP
+	D=M
+	@LCL
+	M=D
+	@Sys.init
+	0;JMP
+(RETURN_ADDRESS_0)
 //function Class1.set 0
 (Class1.set)
 
@@ -14,8 +69,9 @@
 	M=M+1
 
 //pop static 0
-	@stat_0
+	@Class10
 	D=A
+	D=M+D
 	@addr
 	M=D
 	@SP
@@ -38,8 +94,9 @@
 	M=M+1
 
 //pop static 1
-	@stat_1
+	@Class11
 	D=A
+	D=M+D
 	@addr
 	M=D
 	@SP
@@ -121,96 +178,9 @@
 (Class1.get)
 
 //push static 0
-	@stat_0
-	D=M
-	@SP
-	A=M
-	M=D
-	@SP
-	M=M+1
-
-//push static 1
-	@stat_1
-	D=M
-	@SP
-	A=M
-	M=D
-	@SP
-	M=M+1
-
-//sub
-	@SP
-	AM=M-1
-	D=M
-	A=A-1
-	M=M-D
-
-//return
-	@LCL
-	D=M
-	@endframe
-	M=D
-	@endframe
-	D=M
-	@5
-	D=D-A
-	A=D
-	D=M
-	@retaddr
-	M=D
-	@SP
-	AM=M-1
-	D=M
-	@ARG
-	A=M
-	M=D
-	@ARG
-	D=M+1
-	@SP
-	M=D
-	@endframe
-	D=M
-	@1
-	D=D-A
-	A=D
-	D=M
-	@THAT
-	M=D
-	@endframe
-	D=M
-	@2
-	D=D-A
-	A=D
-	D=M
-	@THIS
-	M=D
-	@endframe
-	D=M
-	@3
-	D=D-A
-	A=D
-	D=M
-	@ARG
-	M=D
-	@endframe
-	D=M
-	@4
-	D=D-A
-	A=D
-	D=M
-	@LCL
-	M=D
-	@retaddr
-	A=M
-	0;JMP
-
-//function Class2.set 0
-(Class2.set)
-
-//push argument 0
-	@0
+	@Class10
 	D=A
-	@ARG
+	@Class10
 	A=M+D
 	D=M
 	@SP
@@ -219,124 +189,11 @@
 	@SP
 	M=M+1
 
-//pop static 0
-	@stat_0
-	D=A
-	@addr
-	M=D
-	@SP
-	AM=M-1
-	D=M
-	@addr
-	A=M
-	M=D
-
-//push argument 1
-	@1
-	D=A
-	@ARG
-	A=M+D
-	D=M
-	@SP
-	A=M
-	M=D
-	@SP
-	M=M+1
-
-//pop static 1
-	@stat_1
-	D=A
-	@addr
-	M=D
-	@SP
-	AM=M-1
-	D=M
-	@addr
-	A=M
-	M=D
-
-//push constant 0
-	@0
-	D=A
-	@SP
-	A=M
-	M=D
-	@SP
-	M=M+1
-
-//return
-	@LCL
-	D=M
-	@endframe
-	M=D
-	@endframe
-	D=M
-	@5
-	D=D-A
-	A=D
-	D=M
-	@retaddr
-	M=D
-	@SP
-	AM=M-1
-	D=M
-	@ARG
-	A=M
-	M=D
-	@ARG
-	D=M+1
-	@SP
-	M=D
-	@endframe
-	D=M
-	@1
-	D=D-A
-	A=D
-	D=M
-	@THAT
-	M=D
-	@endframe
-	D=M
-	@2
-	D=D-A
-	A=D
-	D=M
-	@THIS
-	M=D
-	@endframe
-	D=M
-	@3
-	D=D-A
-	A=D
-	D=M
-	@ARG
-	M=D
-	@endframe
-	D=M
-	@4
-	D=D-A
-	A=D
-	D=M
-	@LCL
-	M=D
-	@retaddr
-	A=M
-	0;JMP
-
-//function Class2.get 0
-(Class2.get)
-
-//push static 0
-	@stat_0
-	D=M
-	@SP
-	A=M
-	M=D
-	@SP
-	M=M+1
-
 //push static 1
-	@stat_1
+	@Class11
+	D=A
+	@Class11
+	A=M+D
 	D=M
 	@SP
 	A=M
@@ -432,7 +289,7 @@
 	M=M+1
 
 //call Class1.set 2
-	@RETURN_ADDRESS_0
+	@RETURN_ADDRESS_1
 	D=A
 	@SP
 	A=M
@@ -481,7 +338,7 @@
 	M=D
 	@Class1.set
 	0;JMP
-(RETURN_ADDRESS_0)
+(RETURN_ADDRESS_1)
 
 //pop temp 0
 	@0
@@ -516,7 +373,7 @@
 	M=M+1
 
 //call Class2.set 2
-	@RETURN_ADDRESS_1
+	@RETURN_ADDRESS_2
 	D=A
 	@SP
 	A=M
@@ -565,7 +422,7 @@
 	M=D
 	@Class2.set
 	0;JMP
-(RETURN_ADDRESS_1)
+(RETURN_ADDRESS_2)
 
 //pop temp 0
 	@0
@@ -582,7 +439,7 @@
 	M=D
 
 //call Class1.get 0
-	@RETURN_ADDRESS_2
+	@RETURN_ADDRESS_3
 	D=A
 	@SP
 	A=M
@@ -631,10 +488,10 @@
 	M=D
 	@Class1.get
 	0;JMP
-(RETURN_ADDRESS_2)
+(RETURN_ADDRESS_3)
 
 //call Class2.get 0
-	@RETURN_ADDRESS_3
+	@RETURN_ADDRESS_4
 	D=A
 	@SP
 	A=M
@@ -683,12 +540,226 @@
 	M=D
 	@Class2.get
 	0;JMP
-(RETURN_ADDRESS_3)
+(RETURN_ADDRESS_4)
 
 //label WHILE
 (WHILE)
 
 //goto WHILE
 	@WHILE
+	0;JMP
+
+//function Class2.set 0
+(Class2.set)
+
+//push argument 0
+	@0
+	D=A
+	@ARG
+	A=M+D
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+
+//pop static 0
+	@Class20
+	D=A
+	D=M+D
+	@addr
+	M=D
+	@SP
+	AM=M-1
+	D=M
+	@addr
+	A=M
+	M=D
+
+//push argument 1
+	@1
+	D=A
+	@ARG
+	A=M+D
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+
+//pop static 1
+	@Class21
+	D=A
+	D=M+D
+	@addr
+	M=D
+	@SP
+	AM=M-1
+	D=M
+	@addr
+	A=M
+	M=D
+
+//push constant 0
+	@0
+	D=A
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+
+//return
+	@LCL
+	D=M
+	@endframe
+	M=D
+	@endframe
+	D=M
+	@5
+	D=D-A
+	A=D
+	D=M
+	@retaddr
+	M=D
+	@SP
+	AM=M-1
+	D=M
+	@ARG
+	A=M
+	M=D
+	@ARG
+	D=M+1
+	@SP
+	M=D
+	@endframe
+	D=M
+	@1
+	D=D-A
+	A=D
+	D=M
+	@THAT
+	M=D
+	@endframe
+	D=M
+	@2
+	D=D-A
+	A=D
+	D=M
+	@THIS
+	M=D
+	@endframe
+	D=M
+	@3
+	D=D-A
+	A=D
+	D=M
+	@ARG
+	M=D
+	@endframe
+	D=M
+	@4
+	D=D-A
+	A=D
+	D=M
+	@LCL
+	M=D
+	@retaddr
+	A=M
+	0;JMP
+
+//function Class2.get 0
+(Class2.get)
+
+//push static 0
+	@Class20
+	D=A
+	@Class20
+	A=M+D
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+
+//push static 1
+	@Class21
+	D=A
+	@Class21
+	A=M+D
+	D=M
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+
+//sub
+	@SP
+	AM=M-1
+	D=M
+	A=A-1
+	M=M-D
+
+//return
+	@LCL
+	D=M
+	@endframe
+	M=D
+	@endframe
+	D=M
+	@5
+	D=D-A
+	A=D
+	D=M
+	@retaddr
+	M=D
+	@SP
+	AM=M-1
+	D=M
+	@ARG
+	A=M
+	M=D
+	@ARG
+	D=M+1
+	@SP
+	M=D
+	@endframe
+	D=M
+	@1
+	D=D-A
+	A=D
+	D=M
+	@THAT
+	M=D
+	@endframe
+	D=M
+	@2
+	D=D-A
+	A=D
+	D=M
+	@THIS
+	M=D
+	@endframe
+	D=M
+	@3
+	D=D-A
+	A=D
+	D=M
+	@ARG
+	M=D
+	@endframe
+	D=M
+	@4
+	D=D-A
+	A=D
+	D=M
+	@LCL
+	M=D
+	@retaddr
+	A=M
 	0;JMP
 
