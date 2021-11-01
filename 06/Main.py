5ce528463cab0f9d6af4666d3f226fc50fefc14e
Ron <Ronkatz29@gmail.com>
2021-11-01 10:09:03 +0200

    final project 6 after drive 6

--- 06/Main.py
+++ 06/Main.py
@@ -20,28 +20,10 @@ def assemble_file(
         input_file (typing.TextIO): the file to assemble.
         output_file (typing.TextIO): writes all output to this file.
     """
-    # Your code goes here!
-    #
-    # You should use the two-pass implementation suggested in the book:
-    #
-    # *Initialization*
-    # Initialize the p_two.symbol() table with all the predefined symbols and their
-    # pre-allocated RAM addresses, according to section 6.2.3 of the book.
-    #
+    # generator symbol table
     symbol_table = SymbolTable()
-    # *First Pass* Go through the entire assembly program, line by line,
-    # and build the p_two.symbol() table without generating any code. As you march
-    # through the program lines, keep a running number recording the ROM
-    # address into which the current command will be eventually loaded. This
-    # number starts at 0 and is incremented by 1 whenever a C-instruction or
-    # an A-instruction is encountered, but does not change when a label
-    # pseudo-command or a comment is encountered. Each time a pseudo-command
-    # (Xxx) is encountered, add a new entry to the p_two.symbol() table, associating
-    # Xxx with the ROM address that will eventually store the next command
-    # in the program. This pass results in entering all the program’s labels
-    # along with their ROM addresses into the p_two.symbol() table. The program’s
-    # variables are handled in the second pass.
-    #
+
+    # first pass
     real_commends = 0
     p_one = Parser(input_file)
     while p_one.has_more_commands():
@@ -51,22 +33,11 @@ def assemble_file(
                 symbol_table.add_entry(p_one.symbol(), real_commends)
         else:
             real_commends += 1
-    # *Second Pass*
-    # Now go again through the entire program, and parse each line.
-    # Each time a symbolic A-instruction is encountered, namely, @Xxx where Xxx
-    # is a p_two.symbol() and not a number, look up Xxx in the p_two.symbol() table.
-    # If the p_two.symbol() is found in the table, replace it with its numeric meaning
-    # and complete the command’s translation.
-    # If the p_two.symbol() is not found in the table, then it must represent a new
-    # variable. To handle it, add the pair (Xxx,n) to the p_two.symbol() table, where n
-    # is the next available RAM address, and complete the command’s translation.
-    # The allocated RAM addresses are consecutive numbers, starting at address
-    # 16 (just after the addresses allocated to the predefined symbols).
-    # After the command is translated, write the translation to the output file.
+
+    # second pass
     input_file.seek(0)
     p_two = Parser(input_file)
     n = 16
-    # output_file.write("Today \n")
     coder = Code()
 
     while p_two.has_more_commands():
@@ -87,7 +58,7 @@ def assemble_file(
             bin_num += coder.comp(p_two.comp())
             bin_num += coder.dest(p_two.dest())
             bin_num += coder.jump(p_two.jump())
-            output_file.write(bin_num )
+            output_file.write(bin_num)
 
         if(p_two.has_more_commands() and p_two.command_type() != "L_COMMAND"):
             output_file.write("\n")
